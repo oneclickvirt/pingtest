4d2bdbaf293ac7dc0eea5264e63985a228c76b55
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-07-03 04:36:09 +0000

    update

--- pt/utils.go
+++ pt/utils.go
@@ -1,7 +1,9 @@
 package pt
 
 import (
+	"encoding/csv"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/imroc/req/v3"
@@ -36,3 +38,65 @@ func getData(endpoint string) string {
 	}
 	return ""
 }
+
+func parseCSVData(data, platform string) []model.Server {
+	var servers []model.Server
+	r := csv.NewReader(strings.NewReader(data))
+	records, err := r.ReadAll()
+	if err != nil {
+		if model.EnableLoger {
+			Logger.Info(err.Error())
+		}
+		return servers
+	}
+	if len(records) > 0 && (records[0][6] == "country_code" || records[0][1] == "country_code") {
+		records = records[1:]
+	}
+	if platform == "net" {
+		for _, record := range records {
+			if len(record) >= 8 {
+				servers = append(servers, model.Server{
+					Name: record[3],
+					IP:   record[4],
+					Port: record[6],
+				})
+			}
+		}
+	} else if platform == "cn" {
+		for _, record := range records {
+			if len(record) >= 8 {
+				servers = append(servers, model.Server{
+					Name: record[10] + record[8],
+					IP:   strings.Split(record[5], ":")[0],
+					Port: strings.Split(record[5], ":")[1],
+				})
+			}
+		}
+	}
+	return servers
+}
+
+func getServers(operator string) []model.Server {
+    netList := []string{model.NetCMCC, model.NetCT, model.NetCU}
+    cnList := []string{model.CnCMCC, model.CnCT, model.CnCU}
+    var servers []model.Server
+    // 定义一个函数来获取数据并解析
+    appendData := func(data1, data2 string) {
+        if data1 != "" {
+            servers = append(servers, parseCSVData(data1, "net")...)
+        }
+        if data2 != "" {
+            servers = append(servers, parseCSVData(data2, "cn")...)
+        }
+    }
+    switch operator {
+    case "cmcc":
+        appendData(getData(netList[0]), getData(cnList[0]))
+    case "ct":
+        appendData(getData(netList[1]), getData(cnList[1]))
+    case "cu":
+        appendData(getData(netList[2]), getData(cnList[2]))
+    }
+    return servers
+}
+
